cb14405f4bd5e5dd95d13c5ab9e91c65c1eedb5b
Budi-Laptop <budi.salah@outlook.com>
2022-01-28 11:24:56 +0200

    finish load more btn in homepage

--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -1,10 +1,15 @@
-angular
-  .module('appModule')
-  .controller('homeController', homePageController);
+/* eslint-disable radix */
+angular.module('appModule').controller('homeController', homePageController);
 
 function homePageController(Employees) {
   const homePageVm = this;
   homePageVm.employees = [];
+  homePageVm.loading = true;
+  homePageVm.currentPage = 1;
+  homePageVm.pages = undefined;
+  homePageVm.showLoadBtn = function () {
+    return parseInt(homePageVm.currentPage) <= parseInt(homePageVm.pages);
+  };
 
   activate();
 
@@ -12,6 +17,34 @@ function homePageController(Employees) {
     Employees.getEmployees()
       .then(({ data }) => {
         homePageVm.employees = homePageVm.employees.concat(data.employees);
+        homePageVm.pages = data.pages;
+        homePageVm.currentPage += 1;
+      })
+      .catch((error) => {
+        // TBD: show error notification
+        console.error(error);
+      })
+      .finally(() => {
+        homePageVm.loading = false;
+        homePageVm.showLoadBtn();
       });
   }
+
+  homePageVm.loadMoreHandler = function () {
+    homePageVm.loading = true;
+
+    Employees.loadMoreEmployees(homePageVm.currentPage)
+      .then(({ data }) => {
+        homePageVm.employees = homePageVm.employees.concat(data.employees);
+        homePageVm.currentPage += 1;
+      })
+      .catch((error) => {
+        // TBD: show error notification
+        console.error(error);
+      })
+      .finally(() => {
+        homePageVm.loading = false;
+        homePageVm.showLoadBtn();
+      });
+  };
 }
